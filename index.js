724dab54c3e10ea549bcf7be9b7047b32614bdb8
Emir Maljanovic <emir_maljan@hotmail.com>
2019-04-11 15:34:19 +0200

    Structured data according to document requirements

--- index.js
+++ index.js
@@ -2,22 +2,16 @@ const admin = require('firebase-admin');
 const serviceAccount = require('./config/firebase.json');
 
 // Libs
+const generatePDF = require('./lib/pdfGenerator');
 const generateChart = require('./lib/chartGenerator');
 
 // Chart configs
 const areaChartSpec = require('./config/area-chart.json');
 const lineChartSpec = require('./config/line-chart.json');
 
-const queryData = () => {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://twitchsniffer-25e09.firebaseio.com'
-  });
 
-  const rootRef = admin.database().ref('streams');
-
-  rootRef.on('value', (snapshot) => {
-    if (!snapshot) { return; }
+const arrangeData = (snapshot) => {
+  if (!snapshot) { return; }
 
     const data = snapshot.val();
     const values = [];
@@ -29,10 +23,29 @@ const queryData = () => {
 
     lineChartSpec.data[0].values = values;
 
-    generateChart(lineChartSpec);
+    generateChart(lineChartSpec, (err) => {
+      if (err) throw err;
+      console.log(`
+      ----------------------------
+            Chart generated.
+      ----------------------------
+      `);
+
+      generatePDF(data, values);
+    });
 
     admin.app().delete();
-  }, (errorObject) => {
+};
+
+const queryData = () => {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://twitchsniffer-25e09.firebaseio.com'
+  });
+
+  const rootRef = admin.database().ref('streams');
+
+  rootRef.on('value', arrangeData, (errorObject) => {
     console.log('The read failed:', errorObject.code);
   });
 };
